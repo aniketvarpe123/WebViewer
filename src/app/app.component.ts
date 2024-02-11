05801bf42080c195a8efbc67205797e3cbe31d7b
Aniket varpe <aniket.varpe@mgstech.in>
2024-02-11 18:49:07 +0530

    11Feb2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,11 @@
-/* 
-   Aniket Varpe: 07 March,2024 
-   Apryse WebViewer Links: https://docs.apryse.com/documentation/web/guides/hiding-elements/
-*/
-
 import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { HttpHeaders, HttpClient, HttpErrorResponse } from '@angular/common/http';
 import WebViewer, { WebViewerInstance } from "@pdftron/webviewer";
-import { Subject } from "rxjs";
 import { PDFNet } from '@pdftron/pdfnet-node';
+import { Subject } from "rxjs";
 
+//const { PDFNet } = require('@pdftron/pdfnet-node');
 // Declare CSConfig globally or define it somewhere in your code
-
 const CSConfig = {
   CSUrl: 'http://eimsdemo.mannaicorp.com.qa/otcs/cs.exe', // Replace with your content server URL
   Otcsticket: '',  // Add the authentication token property
@@ -59,7 +54,6 @@ export class AppComponent implements AfterViewInit {
       }
 
       const authData = await authResponse.json();
-
       CSConfig.Otcsticket = authData['ticket'];  // Set the authentication token
 
       // Logging the authentication token for debugging purposes
@@ -67,8 +61,7 @@ export class AppComponent implements AfterViewInit {
 
       // Continue with WebViewer initialization
       const fileBlob = await this.generateAuthenticatedFileBlob(CSConfig);
-    
-     
+
       // Determine the file extension based on its MIME type
       const fileExtension = fileBlob.type.startsWith('application/pdf') ? 'pdf' : 'docx';
 
@@ -85,30 +78,28 @@ export class AppComponent implements AfterViewInit {
 
         // Hide the left panel and left panel button
         instance.UI.disableElements(['leftPanel', 'leftPanelButton']);
-        instance.UI.disableElements(['menuButton', 'menuButton']);
-        instance.UI.disableElements(['viewControlsButton', 'viewControlsButton']);
+        instance.UI.disableElements(['menuButton', 'menuButton'])
 
         // Add header button for exporting annotations
-        instance.UI.setHeaderItems(header => {
-          header.push({
-            //type: 'actionButton',
-           // img: '...', // Replace with the appropriate image URL or icon class
-            onClick: async () => {
-              const doc = Core.documentViewer.getDocument();
-              const annotationManager = Core.annotationManager;
-              const xfdfString = await annotationManager.exportAnnotations();
-              const data = await doc.getFileData({
-                xfdfString
-              });
-              const arr = new Uint8Array(data);
-              const blob = new Blob([arr], { type: 'application/pdf' }); // Adjust for DOCX file
+        // instance.UI.setHeaderItems(header => {
+        //   header.push({
+        //     type: 'actionButton',
+        //     img: '...', // Replace with the appropriate image URL or icon class
+        //     onClick: async () => {
+        //       const doc = Core.documentViewer.getDocument();
+        //       const annotationManager = Core.annotationManager;
+        //       const xfdfString = await annotationManager.exportAnnotations();
+        //       const data = await doc.getFileData({
+        //         xfdfString
+        //       });
+        //       const arr = new Uint8Array(data);
+        //       const blob = new Blob([arr], { type: 'application/pdf' }); // Adjust for DOCX file
 
-              // Trigger download
-              // this.downloadFile(blob, `02_Project.${fileExtension}`);
-              this.downloadFile(blob, `02_Project`);
-            }
-          });
-        });
+        //       // Trigger download
+        //       this.downloadFile(blob, `02_Project.${fileExtension}`);
+        //     }
+        //   });
+        // });
 
         // Add event listener for annotations loaded
         Core.documentViewer.addEventListener('annotationsLoaded', () => {
@@ -131,76 +122,106 @@ export class AppComponent implements AfterViewInit {
     }
   }
 
-  async generateAuthenticatedFileBlob(CSConfig: any): Promise<Blob> {
-    console.log('AuthToken - generateAuthenticatedFileBlob:', CSConfig.Otcsticket);
-    const url = `${CSConfig.CSUrl}/api/v1/nodes/3161737/content?Otcsticket=${CSConfig.Otcsticket}`;
+ 
+async generateAuthenticatedFileBlob(CSConfig: any): Promise<Blob> {
+  console.log('AuthToken:', CSConfig.Otcsticket);
+  const url = `${CSConfig.CSUrl}/api/v1/nodes/3161277/content?Otcsticket=${CSConfig.Otcsticket}`;
 
-    // Only make the request if the document is not already loaded
-    if (!this.isDocumentLoaded) {
-      const headers = new HttpHeaders({
-        'Otcsticket': CSConfig.Otcsticket,
-      });
+  // Only make the request if the document is not already loaded
+  if (!this.isDocumentLoaded) {
+    const headers = new HttpHeaders({
+      'Otcsticket': CSConfig.Otcsticket,
+    });
 
-      try {
-        const response = await this.http.get(url, { headers, responseType: 'blob' }).toPromise();
-        console.log("Line 142 :",response);
-
-        return response as Blob;
-      } catch (error) {
-        if (error instanceof HttpErrorResponse) {
-          if (error.status === 401) {
-            console.error('401 Unauthorized - Invalid authentication token or credentials');
-          } else {
-            console.error('Error:', error);
-          }
-        } else {
-          console.error('Unexpected error:', error);
-        }
-
-        throw error; // Re-throw the error to propagate it further if needed
+    try {
+      const response = await this.http.get(url, { headers, responseType: 'blob' }).toPromise();
+      // Check if the response is defined and its type is a PDF
+      if (response && response.type === 'application/pdf') {
+        console.log('Fetched content is a PDF');
+      } else if (response && response.type === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
+        console.log('Fetched content is a DOCX');
+        // Convert DOCX to PDF
+        const docxBlobfile = new Blob([response], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
+       // const respo= this.createBlobUrl(response);
+        const pdfBlob = await this.convertDocxToPdf(docxBlobfile);
+        console.log('Converted DOCX to PDF');
+        return pdfBlob;
+      } else {
+        console.log('Fetched content is of unknown type');
       }
+      return response as Blob;
+    } catch (error) {
+      if (error instanceof HttpErrorResponse) {
+        if (error.status === 401) {
+          console.error('401 Unauthorized - Invalid authentication token or credentials');
+        } else {
+          console.error('Error:', error);
+        }
+      } else {
+        console.error('Unexpected error:', error);
+      }
+
+      throw error; // Re-throw the error to propagate it further if needed
     }
-
-    // If the document is already loaded, return an empty Blob or handle it as needed
-    return new Blob();
   }
 
-  createBlobUrl(blob: Blob): string {
-    return URL.createObjectURL(blob);
-  }
+  // If the document is already loaded, return an empty Blob or handle it as needed
+  return new Blob();
+}
 
-  // Method to trigger the download
-  downloadFile(blob: Blob, filename: string): void {
-    const a = document.createElement('a');
-    a.href = URL.createObjectURL(blob);
-    a.download = filename;
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-  }
+async convertDocxToPdf(docxBlob: Blob): Promise<Blob> {
+try {
+    // Read the contents of the Blob as an ArrayBuffer
+    const arrayBuffer = await docxBlob.arrayBuffer();
+
+    // Perform the conversion with the ArrayBuffer
+    const pdfBuffer = await PDFNet.Convert.office2PDFBuffer(arrayBuffer);
+
+    // Return the converted PDF as a Blob
+    return new Blob([pdfBuffer], { type: 'application/pdf' });
+} catch (error) {
+    console.error('Error converting DOCX to PDF:', error);
+    throw error; // Re-throw the error to propagate it further if needed
+}
+}
+
+createBlobUrl(blob: Blob): string {
+  return URL.createObjectURL(blob);
+}
+
+
+  // // Method to trigger the download
+  // downloadFile(blob: Blob, filename: string): void {
+  //   const a = document.createElement('a');
+  //   a.href = URL.createObjectURL(blob);
+  //   a.download = filename;
+  //   document.body.appendChild(a);
+  //   a.click();
+  //   document.body.removeChild(a);
+  // }
 
   // Method to save changes as PDF (called on "Save" button click)
-  saveChangesAsPDF(): void {
-    // Add any logic here if needed before triggering the export
-    // For example, you may want to perform some actions or validations before exporting annotations.
+  // saveChangesAsPDF(): void {
+  //   // Add any logic here if needed before triggering the export
+  //   // For example, you may want to perform some actions or validations before exporting annotations.
 
-    // Trigger the export of annotations (same logic as in the header button)
-    if (this.wvInstance) {
-      const { Core } = this.wvInstance;
-      const doc = Core.documentViewer.getDocument();
-      const annotationManager = Core.annotationManager;
-      annotationManager.exportAnnotations().then(async (xfdfString) => {
-        const data = await doc.getFileData({
-          xfdfString
-        });
-        const arr = new Uint8Array(data);
-        const blob = new Blob([arr], { type: 'application/pdf' });
+  //   // Trigger the export of annotations (same logic as in the header button)
+  //   if (this.wvInstance) {
+  //     const { Core } = this.wvInstance;
+  //     const doc = Core.documentViewer.getDocument();
+  //     const annotationManager = Core.annotationManager;
+  //     annotationManager.exportAnnotations().then(async (xfdfString) => {
+  //       const data = await doc.getFileData({
+  //         xfdfString
+  //       });
+  //       const arr = new Uint8Array(data);
+  //       const blob = new Blob([arr], { type: 'application/pdf' });
 
-        // Trigger download
-        this.downloadFile(blob, 'document.pdf');
-      });
-    }
-  }
+  //       // Trigger download
+  //       this.downloadFile(blob, 'document.pdf');
+  //     });
+  //   }
+  // }
 
   addVersion(): void {
     if (this.wvInstance) {
@@ -213,27 +234,28 @@ export class AppComponent implements AfterViewInit {
           xfdfString
         });
 
-        const nodeId = '3161737'; // Replace with the actual nodeId
+        const nodeId = '3161277'; // Replace with the actual nodeId
 
         // Convert ArrayBuffer to Uint8Array
         const uint8ArrayData = new Uint8Array(data);
 
         // Get the original file
-        // const originalFile = (await this.http.get(`${CSConfig.CSUrl}/api/v1/nodes/${nodeId}`, {
-        //   headers: {
-        //     'Otcsticket': CSConfig.Otcsticket,
-        //   },
-        //   responseType: 'arraybuffer'
-        // }).toPromise()) as ArrayBuffer;
+        const originalFile = (await this.http.get(`${CSConfig.CSUrl}/api/v1/nodes/${nodeId}`, {
+          headers: {
+            'Otcsticket': CSConfig.Otcsticket,
+          },
+          responseType: 'arraybuffer'
+        }).toPromise()) as ArrayBuffer;
 
         // Call a method to send the file and versioning details to the content server
-        this.addVersionToContentServer(nodeId, uint8ArrayData);
+        this.addVersionToContentServer(nodeId, originalFile, uint8ArrayData);
       });
     }
   }
 
   private async addVersionToContentServer(
-    nodeId: string,   
+    nodeId: string,
+    originalFile: ArrayBuffer,
     fileData: Uint8Array
   ): Promise<void> {
     const contentServerUrl = `${CSConfig.CSUrl}/api/v2/nodes/${nodeId}/versions`;
